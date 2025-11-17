b4e80e115357cddf1161dbaaad85fcdf7c7a36f2
Kryštof Hrdý <132806992+KikoStudios@users.noreply.github.com>
2025-11-17 14:42:39 +0100

    Update Linux download URL in install-config.sh

--- bin/install-config.sh
+++ bin/install-config.sh
@@ -13,6 +13,6 @@ ASSET_MAC_ARM64="Veb_arm64_Mac_B0.0.1"       # example — adjust if you build M
 ASSET_WIN_X64="Veb_x64_Windows_B0.0.1.exe"   # Windows x64 example
 
 # Final download URLs
-URL_LINUX_X64="${BASE_URL}/${ASSET_LINUX_X64}"
+URL_LINUX_X64="https://github.com/KikoStudios/veb-install/releases/download/beta/veb"
 URL_MAC_ARM64="${BASE_URL}/${ASSET_MAC_ARM64}"
 URL_WIN_X64="${BASE_URL}/${ASSET_WIN_X64}"
